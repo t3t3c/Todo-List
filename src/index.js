885d6ca1cdc1930f6cf4d4badc7226f4a4765057
t3t3c <65117237+t3t3c@users.noreply.github.com>
2022-08-08 14:03:24 +0200

    added working style.css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("test");
+import './style.css';
+
+const TodoItem = (title, description, dueDate, priority) => {
+  return { title, description, dueDate, priority };
+};
+const Project = (todoItemsArray) => {
+  return { todoItemsArray };
+};
+
+const DOMmodify = () => {
+  // empty
+};
+
+// UI
+// view all projects
+// view all todos in each project
+// expand a single todo
+// delete a todo
