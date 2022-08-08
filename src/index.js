1138594d97579f62e8dd4608e26b2f141a81c75f
t3t3c <65117237+t3t3c@users.noreply.github.com>
2022-08-08 21:27:51 +0200

    added more styling

--- src/index.js
+++ src/index.js
@@ -12,13 +12,6 @@ const DOMmodify = () => {
   // empty
 };
 
-const element = document.querySelector('#content');
-const btn = document.createElement('button');
-
-btn.innerHTML = 'Click me and check the console!';
-btn.onclick = printMe;
-element.appendChild(btn);
-asdf.appendChild('asdf')
 // UI
 // view all projects
 // view all todos in each project
